2f54fbbba2e384225190819b0ac0005280500c8a
5pe3dy <f.v.limburg@gmail.com>
2023-07-20 21:18:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# EventApp
+
+This app has been vreated with React and vite. 
+The server needs to run on localhost:3333 so that the database wil work. 
+
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import { Box, Flex, List, ListItem, Text } from "@chakra-ui/react";
+
+export const Navigation = () => {
+  return (
+    <Box
+      as="nav"
+      bg="bg-surface"
+      boxShadow="md"
+      h={"50px"}
+      backgroundColor={"#1a1438"}
+      style={{ position: "sticky", top: 0, zIndex: 1 }}
+    >
+      <Flex justifyContent={"space-evenly"} padding={"10px"}>
+        <List>
+          <Link to="/">
+            <Text
+              color={"white"}
+              fontFamily={"Poppins"}
+              fontSize={"20px"}
+              fontWeight={"bold"}
+              _hover={{ color: "gray.600" }}
+            >
+              Events List
+            </Text>
+            <ListItem borderBottomWidth="1px" borderColor="white"></ListItem>
+          </Link>
+        </List>
+      </Flex>
+    </Box>
+  );
+};
